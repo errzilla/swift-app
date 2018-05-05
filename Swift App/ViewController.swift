e25f8a70cf0763b097bef88b6471eec8024f4670
Erza Carlsson <erzami@icloud.com>
2018-05-05 04:29:30 -0700

    Boolean Lesson

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,19 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var Textbox1: UITextField!
+    
+    @IBOutlet weak var Textbox2: UITextField!
+    
     @IBOutlet weak var theLabel: UILabel!
     
     var tapCount = 0
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        tapCount = tapCount + 1
-        print(tapCount)
+        let addition = true
         
-        if tapCount >= 10 {
-            theLabel.text = "You tapped the button 10 times!"
+        if addition {
+            theLabel.text = "Answer: \(Double(Textbox1.text!)! + Double(Textbox2.text!)!)"
         }
+        else
+        {
+            theLabel.text = "Answer: \(Double(Textbox1.text!)! - Double(Textbox2.text!)!)"
+        }
+        
+        
+        //print(Textbox1.text!)
+        //print(Textbox2.text!)
+        //tapCount = tapCount + 1
+        //print(tapCount)
+        
+        //if tapCount >= 10 {
+        //   theLabel.text = "You tapped the button 10 times!"
+        //}
         
         //theLabel.text = "Hello Universe!"
         
@@ -34,12 +51,12 @@ class ViewController: UIViewController {
         //self.view.backgroundColor = UIColor.red
         theLabel.text = "Hello Solar System!"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
